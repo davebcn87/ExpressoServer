b7b63bf87163eefd06d9c6172c8296aeed1bb23d
David Cortés <david.cortes.18@gmail.com>
2011-11-25 10:38:29 +0100

    Site de rails. Ara sere el que mes ha treballat. xD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpressoServer::Application.config.secret_token = 'a88a1a41524962143e6c3822bbd1c73a02e13e101b8c4c8f860fcc76263ce7f5586b968f85c953d1860d6dd3f93604b1cc784b11b1abacd4a3adcc9fa7eeb869'
